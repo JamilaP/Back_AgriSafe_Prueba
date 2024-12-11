073f195077ab3be53d23d6ca6508550692891518
Jamila Palacios <jamilapalaciosm@gmail.com>
2024-12-10 22:26:20 -0500

    Revert "ADD package-lock.json, package.json, app.js, diagnosesController.js, upload.js, diagnosesModel.js, diagnosesRouter.js, index.js, usersRouter.js, 8354...6900c1f19.jpg, f59a...1bcba37b3.jpg"
    This reverts commit 20aec0b8b2e2d8d3ef31009328569486675019d1.

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
-// app.js
 const express = require('express');
-const cors = require('cors');
+const cors = require('cors'); // Importa CORS
 const routes = require('./routes');
 require('dotenv').config();
+//require('dotenv').config({ path: `.env.${process.env.NODE_ENV || 'development'}` });
 
 const app = express();
 
@@ -14,22 +14,26 @@ const corsOptions = {
   credentials: true,
 };
 
+// Usa el middleware de CORS
 app.use(cors(corsOptions));
 
 // Middleware para procesar JSON
 app.use(express.json());
 
-// Rutas principales
+// Rutas
 app.use('/api', routes);
+
+// Middleware para subir imagenes
+app.use('/uploads', express.static('uploads'));
 
 // Middleware global para errores
 app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ error: 'Ocurrió un error en el servidor' });
+    console.error(err.stack);
+    res.status(500).json({ error: 'Ocurrió un error en el servidor' });
 });
 
 // Iniciar el servidor
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://host:${PORT}`);
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
--- src/middlewares/upload.js
+++ src/middlewares/upload.js
@@ -1,27 +1,9 @@
 const AWS = require('aws-sdk');
 const multer = require('multer');
-const multerS3 = require('multer-s3');
-require('dotenv').config();
 
-// Configuración de OBS (Huawei Cloud)
-const s3 = new AWS.S3({
-    endpoint: new AWS.Endpoint(process.env.OBS_ENDPOINT), 
-    accessKeyId: process.env.OBS_ACCESS_KEY_ID, 
-    secretAccessKey: process.env.OBS_SECRET_ACCESS_KEY, 
-    s3ForcePathStyle: true,
-});
-
-// Configuración de Multer con OBS
+// Configuración de Multer para almacenamiento en memoria
 const upload = multer({
-    storage: multerS3({
-        s3: s3,
-        bucket: process.env.OBS_BUCKET_NAME,
-        acl: 'public-read', 
-        key: (req, file, cb) => {
-            const uniqueKey = `profile-images/${Date.now()}-${file.originalname}`;
-            cb(null, uniqueKey); // Nombre del archivo en el bucket
-        },
-    }),
+    storage: multer.memoryStorage(),
     fileFilter: (req, file, cb) => {
         if (file.mimetype.startsWith('image/')) {
             cb(null, true);
